e2f32174b5aa40511109298a12b00c052655ded0
Noorts <50550612+Noorts@users.noreply.github.com>
2023-12-15 12:08:11 +0100

    Add system metrics collection

--- utils/master.sh
+++ utils/master.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
-ip a show dev ib0 | grep "inet "
+# Enable/disable system utilization metrics (CPU and memory) collection.
+enable_sys_metric_collection=true
+
+master_ip=$(ip a show dev ib0 | grep "inet " | awk '{print $2}' | awk -F "/" "{print \$1}")
+echo $master_ip
+
+if [ "$enable_sys_metric_collection" = true ]; then
+    output_filename="slurm-${SLURM_JOB_ID}-sys.out"
+
+    # Store job metadata.
+    echo "Job id: ${SLURM_JOB_ID}" > $output_filename
+    echo "Master ip: $master_ip" >> $output_filename
+    echo "--" >> $output_filename
+
+    # Collect system utilization metrics (CPU and memory).
+    top -b -d 5 | grep --line-buffered "top - " -A 4 >> $output_filename &
+fi
 
 poetry run python3 master/run.py
--- utils/start_master.sh
+++ utils/start_master.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-sbatch --time=00:15:00 --job-name=dlsa_m --nodes=1 ~/DLSA/utils/master.sh | awk '{print $4}' | xargs -I {} sh -c 'while [ ! -e "slurm-{}.out" ]; do sleep 1; done; cat "slurm-{}.out" | head --lines 1 | awk "{print \$2}" | awk -F "/" "{print \$1}"'
-
+sbatch_output=$(sbatch --time=00:15:00 --job-name=dlsa_m --nodes=1 ~/DLSA/utils/master.sh)
 if [ $? -eq 0 ]; then
         echo "Master started!"
     else
         echo "Starting master failed."
 fi
+echo $sbatch_output | awk '{print $4}' | xargs -I {} sh -c 'while [ ! -e "slurm-{}.out" ]; do sleep 1; done; cat "slurm-{}.out" | head --lines 1'
+echo $sbatch_output | grep "Submitted batch job"
