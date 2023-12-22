986cb1eb275d5e7a63381aa1989e3b3c3c90577b
haraldurbjarni <haraldurbjarni@gmail.com>
2023-12-22 15:15:47 +0100

    Revert "fix mcpus toserver"
    This reverts commit f661198c2444749c8148491f2731e21b2b509864.

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -58,8 +58,9 @@ fn main() {
     //TODO: benchmark and send ncpus
     let cpus = num_cpus::get();
     println!("Number of cpus: {}", cpus);
-    let cups: f32 = run_benchmark(std::time::Duration::from_secs(1), 2, 4);
+    let cups: f32 = run_benchmark(std::time::Duration::from_secs(1), 2, 4) * 1e-9;
     let cups_int = cups as i32;
+    println!("MCUPS: {}", cups_int);
     let worker_id = client
         .register_worker(MachineSpecs {
             benchmark_result: cups_int,
